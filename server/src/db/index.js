b38d0c3d69a51f20c3493e4c14fe5fc33ffbf3e7
nmukassa1 <78985121+nmukassa1@users.noreply.github.com>
2024-08-12 21:14:58 +0100

    .

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -3,8 +3,8 @@ import config from '../config/env.js';
 
 const db = new pg.Client(config.db);
 
-db.connect()
-  .then(() => console.log('Connected to the database'))
-  .catch(err => console.error('Database connection error:', err.stack));
+// db.connect()
+//   .then(() => console.log('Connected to the database'))
+//   .catch(err => console.error('Database connection error:', err.stack));
 
 export default db;
